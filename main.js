5dfd120341edd3fb909b650add1ceacb33f71475
Alvigiar <officialgmvladmedvedkov@gmail.com>
2021-06-22 09:52:38 +0300

    create burg

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 import './styles/style.css';
 import './styles/main.scss';
+
+
+document.querySelector('.burger__cell').addEventListener('click', () => {
+    document.querySelector('.section-one__list').classList.toggle('active'),
+        document.querySelector('.burger__toggle').classList.toggle('cross'),
+        document.querySelector('body').classList.toggle('off')
+})
+
+document.querySelector('.section-one__list').addEventListener('click', () => {
+    document.querySelector('.section-one__list').classList.remove('active'),
+        document.querySelector('.burger__toggle').classList.remove('cross'),
+        document.querySelector('body').classList.remove('off')
+})
